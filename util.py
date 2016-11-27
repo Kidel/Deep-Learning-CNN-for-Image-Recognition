337e769fc5bcb8a2ee94948b0980c24ddbfddb7b
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2016-11-27 05:23:18 +0000

    started working on cnn for image classification until placeholder variables. Minor adjustments on other files

--- util.py
+++ util.py
@@ -41,6 +41,52 @@ class Util(object):
         # in a single Notebook cell.
         plt.show()
 
+    def plot_images_2(self, images, cls_true, class_names, cls_pred=None, smooth=True):
+        assert len(images) == len(cls_true) == 9
+
+        # Create figure with sub-plots.
+        fig, axes = plt.subplots(3, 3)
+
+        # Adjust vertical spacing if we need to print ensemble and best-net.
+        if cls_pred is None:
+            hspace = 0.3
+        else:
+            hspace = 0.6
+        fig.subplots_adjust(hspace=hspace, wspace=0.3)
+
+        for i, ax in enumerate(axes.flat):
+            # Interpolation type.
+            if smooth:
+                interpolation = 'spline16'
+            else:
+                interpolation = 'nearest'
+
+            # Plot image.
+            ax.imshow(images[i, :, :, :],
+                      interpolation=interpolation)
+
+            # Name of the true class.
+            cls_true_name = class_names[cls_true[i]]
+
+            # Show true and predicted classes.
+            if cls_pred is None:
+                xlabel = "True: {0}".format(cls_true_name)
+            else:
+                # Name of the predicted class.
+                cls_pred_name = class_names[cls_pred[i]]
+
+                xlabel = "True: {0}\nPred: {1}".format(cls_true_name, cls_pred_name)
+
+            # Show the classes as the label on the x-axis.
+            ax.set_xlabel(xlabel)
+
+            # Remove ticks from the plot.
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+        # Ensure the plot is shown correctly with multiple plots
+        # in a single Notebook cell.
+        plt.show()
 
     def print_test_accuracy(self, session, data, x, y_true, y_pred_cls, num_classes, 
                             show_example_errors=False,
