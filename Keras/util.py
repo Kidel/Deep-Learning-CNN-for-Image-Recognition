3afd10bfba69c06807f1b14a4b1583c4158ce1a0
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-02-23 02:53:47 +0100

    small change in architecture

--- Keras/util.py
+++ Keras/util.py
@@ -1,8 +1,10 @@
+# code refactored from Magnus Erik Hvass Pedersen tutorials
+
 import matplotlib.pyplot as plt
-from keras.utils.visualize_util import plot
-from sklearn.metrics import confusion_matrix
+import tensorflow as tf
 import numpy as np
-import os.path
+from sklearn.metrics import confusion_matrix
+import math
 
 class Util(object):
     
@@ -13,51 +15,33 @@ class Util(object):
 
         plt.show()
     
-    def plot_some_images(self, images, img_shape=(28,28), grid_x=3, grid_y=3):
-        fig, axes = plt.subplots(grid_y, grid_x)
-        fig.subplots_adjust(hspace=0.3, wspace=0.3)
-        for i, ax in enumerate(axes.flat):
-            ax.imshow(images[i].reshape(img_shape),
-                       interpolation='nearest',
-                       cmap='binary')
-            ax.set_xticks([])
-            ax.set_yticks([])
-        plt.show()
-    
-    def plot_images(self, images, cls_true, cls_pred=None, img_shape=(28,28), interpolation='none', with_labels=False, labels=[]):
-        if not (len(images) == len(cls_true) == 9):
-            print("\nI cannot plot the graph because there less than 9 points or because the dimension of images and cls_true mismatch")
-            return
+    def plot_images(self, images, cls_true, cls_pred=None, img_size=28, img_shape=(28,28)):
+        assert len(images) == len(cls_true) == 9
+
         # Create figure with 3x3 sub-plots.
         fig, axes = plt.subplots(3, 3)
         fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
         for i, ax in enumerate(axes.flat):
             # Plot image.
-            ax.imshow(images[i].reshape(img_shape), cmap='binary', interpolation=interpolation)
+            ax.imshow(images[i].reshape(img_shape), cmap='binary')
+
             # Show true and predicted classes.
             if cls_pred is None:
-                if not with_labels:
-                    xlabel = "Class: {0}".format(cls_true[i])
-                else: 
-                    xlabel = "Class: {0}".format(labels[cls_true[i]])
+                xlabel = "True: {0}".format(cls_true[i])
             else:
-                if not with_labels:
-                    xlabel = "Class: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
-                else: 
-                    xlabel = "Class: {0}, Pred: {1}".format(labels[cls_true[i]], labels[cls_pred[i]])
+                xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
             # Show the classes as the label on the x-axis.
             ax.set_xlabel(xlabel)
+
             # Remove ticks from the plot.
             ax.set_xticks([])
             ax.set_yticks([])
+
         # Ensure the plot is shown correctly with multiple plots
         # in a single Notebook cell.
         plt.show()
-
-    def maybe_save_network(self, model, model_image_path='images/temp.png'):
-        if not os.path.exists(model_image_path):
-            plot(model, to_file=model_image_path)
-        return model_image_path
     
     def plot_history(self, history, metric='acc', loc='lower right'): 
         # list all data in history
@@ -72,25 +56,336 @@ class Util(object):
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc=loc)
         plt.show()
-        
-    def plot_confusion_matrix(self, y_true, num_classes, cls_pred, with_labels=False, labels=[], text=True):
+
+    def plot_images_2(self, images, cls_true, class_names, cls_pred=None, smooth=True):
+        assert len(images) == len(cls_true) == 9
+
+        # Create figure with sub-plots.
+        fig, axes = plt.subplots(3, 3)
+
+        # Adjust vertical spacing if we need to print ensemble and best-net.
+        if cls_pred is None:
+            hspace = 0.3
+        else:
+            hspace = 0.6
+        fig.subplots_adjust(hspace=hspace, wspace=0.3)
+
+        for i, ax in enumerate(axes.flat):
+            # Interpolation type.
+            if smooth:
+                interpolation = 'spline16'
+            else:
+                interpolation = 'nearest'
+
+            # Plot image.
+            ax.imshow(images[i, :, :, :],
+                      interpolation=interpolation)
+
+            # Name of the true class.
+            cls_true_name = class_names[cls_true[i]]
+
+            # Show true and predicted classes.
+            if cls_pred is None:
+                xlabel = "True: {0}".format(cls_true_name)
+            else:
+                # Name of the predicted class.
+                cls_pred_name = class_names[cls_pred[i]]
+
+                xlabel = "True: {0}\nPred: {1}".format(cls_true_name, cls_pred_name)
+
+            # Show the classes as the label on the x-axis.
+            ax.set_xlabel(xlabel)
+
+            # Remove ticks from the plot.
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+        # Ensure the plot is shown correctly with multiple plots
+        # in a single Notebook cell.
+        plt.show()
+
+    def print_test_accuracy(self, session, data, x, y_true, y_pred_cls, num_classes, 
+                            show_example_errors=False,
+                            show_confusion_matrix=False):
+
+        # Split the test-set into smaller batches of this size.
+        test_batch_size = 256
+
+        # Number of images in the test-set.
+        num_test = len(data.test.images)
+
+        # Allocate an array for the predicted classes which
+        # will be calculated in batches and filled into this array.
+        cls_pred = np.zeros(shape=num_test, dtype=np.int)
+
+        # Now calculate the predicted classes for the batches.
+        # We will just iterate through all the batches.
+        # There might be a more clever and Pythonic way of doing this.
+
+        # The starting index for the next batch is denoted i.
+        i = 0
+
+        while i < num_test:
+            # The ending index for the next batch is denoted j.
+            j = min(i + test_batch_size, num_test)
+
+            # Get the images from the test-set between index i and j.
+            images = data.test.images[i:j, :]
+
+            # Get the associated labels.
+            labels = data.test.labels[i:j, :]
+
+            # Create a feed-dict with these images and labels.
+            feed_dict = {x: images,
+                         y_true: labels}
+
+            # Calculate the predicted class using TensorFlow.
+            cls_pred[i:j] = session.run(y_pred_cls, feed_dict=feed_dict)
+
+            # Set the start-index for the next batch to the
+            # end-index of the current batch.
+            i = j
+
+        # Convenience variable for the true class-numbers of the test-set.
+        cls_true = data.test.cls
+
+        # Create a boolean array whether each image is correctly classified.
+        correct = (cls_true == cls_pred)
+
+        # Calculate the number of correctly classified images.
+        # When summing a boolean array, False means 0 and True means 1.
+        correct_sum = correct.sum()
+
+        # Classification accuracy is the number of correctly classified
+        # images divided by the total number of images in the test-set.
+        acc = float(correct_sum) / num_test
+
+        # Print the accuracy.
+        msg = "Accuracy on Test-Set: {0:.1%} ({1} / {2})"
+        print(msg.format(acc, correct_sum, num_test))
+
+        # Plot some examples of mis-classifications, if desired.
+        if show_example_errors:
+            print("Example errors:")
+            self.plot_example_errors(data=data, cls_pred=cls_pred, correct=correct)
+
+        # Plot the confusion matrix, if desired.
+        if show_confusion_matrix:
+            print("Confusion Matrix:")
+            self.plot_confusion_matrix(data=data, num_classes=num_classes, cls_pred=cls_pred)
+
+
+    def plot_confusion_matrix(self, data, num_classes, cls_pred):
+        # This is called from print_test_accuracy() below.
+
+        # cls_pred is an array of the predicted class-number for
+        # all images in the test-set.
+
+        # Get the true classifications for the test-set.
+        cls_true = data.test.cls
+
         # Get the confusion matrix using sklearn.
-        cm = confusion_matrix(y_true=y_true,
+        cm = confusion_matrix(y_true=cls_true,
                               y_pred=cls_pred)
+
+        # Print the confusion matrix as text.
+        print(cm)
+
         # Plot the confusion matrix as an image.
         plt.matshow(cm)
+
         # Make various adjustments to the plot.
         plt.colorbar()
         tick_marks = np.arange(num_classes)
-        if with_labels:
-            plt.xticks(range(num_classes), labels, rotation='vertical')
-            plt.yticks(range(num_classes), labels)
-        else:
-            plt.xticks(tick_marks, range(num_classes))
-            plt.yticks(tick_marks, range(num_classes))
+        plt.xticks(tick_marks, range(num_classes))
+        plt.yticks(tick_marks, range(num_classes))
         plt.xlabel('Predicted')
         plt.ylabel('True')
+
+        # Ensure the plot is shown correctly with multiple plots
+        # in a single Notebook cell.
         plt.show()
-        # Print the confusion matrix as text.
-        if text:
-            print(cm)
+
+    def plot_example_errors(self, data, cls_pred, correct):
+        # This function is called from print_test_accuracy() below.
+
+        # cls_pred is an array of the predicted class-number for
+        # all images in the test-set.
+
+        # correct is a boolean array whether the predicted class
+        # is equal to the true class for each image in the test-set.
+
+        # Negate the boolean array.
+        incorrect = (correct == False)
+
+        # Get the images from the test-set that have been
+        # incorrectly classified.
+        images = data.test.images[incorrect]
+
+        # Get the predicted classes for those images.
+        cls_pred = cls_pred[incorrect]
+
+        # Get the true classes for those images.
+        cls_true = data.test.cls[incorrect]
+
+        # Plot the first 9 images.
+        self.plot_images(images=images[0:9],
+                    cls_true=cls_true[0:9],
+                    cls_pred=cls_pred[0:9])
+
+
+    def plot_weights(self, session, weights, img_shape=(28,28)):
+        # Get the values for the weights from the TensorFlow variable.
+        w = session.run(weights)
+
+        # Get the lowest and highest values for the weights.
+        # This is used to correct the colour intensity across
+        # the images so they can be compared with each other.
+        w_min = np.min(w)
+        w_max = np.max(w)
+
+        # Create figure with 3x4 sub-plots,
+        # where the last 2 sub-plots are unused.
+        fig, axes = plt.subplots(3, 4)
+        fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+        for i, ax in enumerate(axes.flat):
+            # Only use the weights for the first 10 sub-plots.
+            if i<10:
+                # Get the weights for the i'th digit and reshape it.
+                # Note that w.shape == (img_size_flat, 10)
+                image = w[:, i].reshape(img_shape)
+
+                # Set the label for the sub-plot.
+                ax.set_xlabel("Weights: {0}".format(i))
+
+                # Plot the image.
+                ax.imshow(image, vmin=w_min, vmax=w_max, cmap='seismic')
+
+            # Remove ticks from each sub-plot.
+            ax.set_xticks([])
+            ax.set_yticks([])
+            
+    def plot_conv_weights(self, session, weights, input_channel=0):
+        # Assume weights are TensorFlow ops for 4-dim variables
+        # e.g. weights_conv1 or weights_conv2.
+
+        # Retrieve the values of the weight-variables from TensorFlow.
+        # A feed-dict is not necessary because nothing is calculated.
+        w = session.run(weights)
+
+        # Get the lowest and highest values for the weights.
+        # This is used to correct the colour intensity across
+        # the images so they can be compared with each other.
+        w_min = np.min(w)
+        w_max = np.max(w)
+
+        # Number of filters used in the conv. layer.
+        num_filters = w.shape[3]
+
+        # Number of grids to plot.
+        # Rounded-up, square-root of the number of filters.
+        num_grids = math.ceil(math.sqrt(num_filters))
+
+        # Create figure with a grid of sub-plots.
+        fig, axes = plt.subplots(num_grids, num_grids)
+
+        # Plot all the filter-weights.
+        for i, ax in enumerate(axes.flat):
+            # Only plot the valid filter-weights.
+            if i<num_filters:
+                # Get the weights for the i'th filter of the input channel.
+                # See new_conv_layer() for details on the format
+                # of this 4-dim tensor.
+                img = w[:, :, input_channel, i]
+
+                # Plot image.
+                ax.imshow(img, vmin=w_min, vmax=w_max,
+                          interpolation='nearest', cmap='seismic')
+
+            # Remove ticks from the plot.
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+        # Ensure the plot is shown correctly with multiple plots
+        # in a single Notebook cell.
+        plt.show()
+        
+        
+    def plot_conv_layer(self, session, x, layer, image):
+        # Assume layer is a TensorFlow op that outputs a 4-dim tensor
+        # which is the output of a convolutional layer,
+        # e.g. layer_conv1 or layer_conv2.
+
+        # Create a feed-dict containing just one image.
+        # Note that we don't need to feed y_true because it is
+        # not used in this calculation.
+        feed_dict = {x: [image]}
+
+        # Calculate and retrieve the output values of the layer
+        # when inputting that image.
+        values = session.run(layer, feed_dict=feed_dict)
+
+        # Number of filters used in the conv. layer.
+        num_filters = values.shape[3]
+
+        # Number of grids to plot.
+        # Rounded-up, square-root of the number of filters.
+        num_grids = math.ceil(math.sqrt(num_filters))
+
+        # Create figure with a grid of sub-plots.
+        fig, axes = plt.subplots(num_grids, num_grids)
+
+        # Plot the output images of all the filters.
+        for i, ax in enumerate(axes.flat):
+            # Only plot the images for valid filters.
+            if i<num_filters:
+                # Get the output image of using the i'th filter.
+                # See new_conv_layer() for details on the format
+                # of this 4-dim tensor.
+                img = values[0, :, :, i]
+
+                # Plot image.
+                ax.imshow(img, interpolation='nearest', cmap='binary')
+
+            # Remove ticks from the plot.
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+        # Ensure the plot is shown correctly with multiple plots
+        # in a single Notebook cell.
+        plt.show()
+        
+        
+    def plot_transfer_values(self, i, images, transfer_values):
+        print("Input image:")
+
+        # Plot the i'th image from the test-set.
+        plt.imshow(images[i], interpolation='nearest')
+        plt.show()
+
+        print("Transfer-values for the image using Inception model:")
+
+        # Transform the transfer-values into an image.
+        img = transfer_values[i]
+        img = img.reshape((32, 64))
+
+        # Plot the image for the transfer-values.
+        plt.imshow(img, interpolation='nearest', cmap='Reds')
+        plt.show()
+        
+    def plot_scatter(self, values, cls, num_classes):
+        # Create a color-map with a different color for each class.
+        import matplotlib.cm as cm
+        cmap = cm.rainbow(np.linspace(0.0, 1.0, num_classes))
+
+        # Get the color for each sample.
+        colors = cmap[cls]
+
+        # Extract the x- and y-values.
+        x = values[:, 0]
+        y = values[:, 1]
+
+        # Plot it.
+        plt.scatter(x, y, color=colors)
+        plt.show()
